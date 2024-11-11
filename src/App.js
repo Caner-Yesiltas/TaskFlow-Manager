f9d9eb49607b2fffb03fe7d2d386f35aa1e99279
CanerYesiltas <caneryesiltas1@gmail.com>
2024-11-10 21:59:20 -0500

    add toggle for completed feature

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import Header from './components/Header';
 import TodoList from './components/TodoList';
 import './styles/App.css';
 import React, { useState } from 'react'
+
+const todoList = [
+  {
+    id:1,
+    text: "TaskFlow Manager",
+    completed: false
+  },
+
+  {
+    id:new Date().getTime(),
+    text:"use uuid",
+    completed: false
+  }
+]
+
 function App() {
-const [todos, setTodos] = useState ([])
-
-
-
+const [todos, setTodos] = useState (todoList)
   return <div className='container'  >
     <Header setTodos ={setTodos} todos={todos} /> 
-    <TodoList todos={todos} />
+    <TodoList todos={todos} setTodos ={setTodos}/>
   </div>;
 }
 export default App;
